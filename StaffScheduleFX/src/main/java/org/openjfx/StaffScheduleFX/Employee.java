686f06ea27b71891a6d365aa530090b3ea65209b
jacobamisano <jpamisano@gmail.com>
2022-01-29 16:09:34 -0600

    Initial commit adding base program to main

---
+++ StaffScheduleFX/src/main/java/org/openjfx/StaffScheduleFX/Employee.java
@@ -0,0 +1,49 @@
+package org.openjfx.StaffScheduleFX;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Employee {
+	
+	private String name;
+	private String id;
+	private String emailAddress;
+	private List<AvailableDays> days;
+	
+	enum AvailableDays {
+		SUNDAY,
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY
+	}
+	
+	//START Constructors 
+	public Employee () {
+		name = null;
+		id = null;
+		emailAddress = null;
+		days = null;
+	}
+	
+	public Employee(String name, String id,
+			String emailAddress, List<AvailableDays> days) {
+		this.name = name;
+		this.id = id;
+		this.emailAddress = emailAddress;
+		this.days = days;
+	}
+	
+	public Employee(String name, String id,
+			String emailAddress) {
+		this.name = name;
+		this.id = id;
+		this.emailAddress = emailAddress;
+		this.days = Arrays.asList(AvailableDays.SUNDAY, AvailableDays.MONDAY, AvailableDays.TUESDAY,
+				AvailableDays.WEDNESDAY, AvailableDays.THURSDAY, AvailableDays.FRIDAY, AvailableDays.SATURDAY);
+	
+	}
+	
+}
