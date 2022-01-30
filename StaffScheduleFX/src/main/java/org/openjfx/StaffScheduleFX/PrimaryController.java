e2965e7a0514c26ed29fbe203bbc1e34ee1aeda1
jacobamisano <jpamisano@gmail.com>
2022-01-30 00:03:37 -0600

    Added frontend for main selection  screen

--- StaffScheduleFX/src/main/java/org/openjfx/StaffScheduleFX/PrimaryController.java
+++ StaffScheduleFX/src/main/java/org/openjfx/StaffScheduleFX/PrimaryController.java
@@ -9,4 +9,30 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+    @FXML
+    private void onBtnAddEmployeeClick() {
+    	
+    }
+    
+    @FXML
+    private void onBtnAddTimeOffClick() {
+    	
+    }
+    
+    @FXML
+    private void onBtnViewEmployeeClick() {
+    	
+    }
+    
+    @FXML
+    private void onBtnEditSchedulePreferencesClick() {
+    	
+    }
+    
+    @FXML
+    private void onBtnBuildScheduleClick() {
+    	
+    }
+    
 }
