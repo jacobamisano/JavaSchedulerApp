9e7c74f7c6de23cda068d44e7ef5ee3cef2c66b3
jacobamisano <jpamisano@gmail.com>
2022-01-31 13:58:46 -0600

    Added basis for TimeOff request class

---
+++ StaffScheduleFX/src/main/java/org/openjfx/StaffScheduleFX/TimeOffRequest.java
@@ -0,0 +1,10 @@
+package org.openjfx.StaffScheduleFX;
+
+import java.time.LocalDateTime;
+
+public class TimeOffRequest {
+	private String employeeRequesting;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+	private LocalDate dateOfRequest;
+}
